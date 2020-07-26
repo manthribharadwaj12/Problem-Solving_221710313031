9cfe7f47c039a7fb01ff396431f1b0eb49f60a33
manthribharadwaj12 <40928485+manthribharadwaj12@users.noreply.github.com>
2020-07-26 12:19:46 +0530

    Delete Perfect subarray.py

--- Perfect subarray.py
+++
@@ -1,8 +0,0 @@
-#https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff43/00000000003381cb
-import math
-t=int(input())
-for i in range(1,t+1):
-    n=int(input())
-    list1=list(map(int,input().split()))
-    sublist = [math.sqrt(sum(list1[k:j]))-math.floor(math.sqrt(sum(list1[k:j])))==0 for k in range(len(list1) + 1) for j in range(k + 1, len(list1) + 1)]
-    print("Case #"+str(i)+": "+str(sublist.count(1)))
